54373738fe6c2754787ef2afb03632d04d784e58
Nguyễn Văn Sáng <nguyensanggb@gmail.com>
2023-03-15 01:41:37 +0700

    update basic routing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
-const hbs  = require('express-handlebars');
+const hbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
 // HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
 
 //Template engine
 app.engine('hbs', hbs.engine({
@@ -18,11 +23,21 @@ app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources/views'))
 
 app.get('/', (req, res) => {
-     res.render('home');
+    res.render('home');
+})
+
+app.get('/news', (req, res) => {
+    res.render('news');
 
 })
-app.get('/tin-tuc', (req, res) => {
-    res.render('news');
+app.get('/search', (req, res) => {
+
+    res.render('search');
+
+})
+app.post('/search', (req, res) => {
+    console.log(req.body.q)
+    res.render('search');
 
 })
 
